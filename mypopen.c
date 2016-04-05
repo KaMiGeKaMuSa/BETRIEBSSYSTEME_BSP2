c03504a83f6a6d5843506883f2fa88c3137d4551
ZerOne10 <michi.mueller1709@gmail.com>
2016-04-05 19:47:58 +0200

    add testcase and wrapper

--- mypopen.c
+++ mypopen.c
@@ -1,20 +1,11 @@
 #include <stdio.h>
-#include <stdlib.h>
-
-int main(int argc, char **argv)
-{
-    FILE *fp;
-    extern FILE *popen();
-    char buff[512];
- 
-    if(!(fp = popen("ls -sail", "r"))){
-        exit(1);
-    }
-
-    while(fgets(buff, sizeof(buff), fp)!=NULL){
-        printf("%s", buff);
-    }
-    pclose(fp);
-
     
+FILE *mypopen(const char *command, const char *type)
+{
+	return popen(command, type);
+}
+	  
+int mypclose(FILE *stream)
+{
+	return pclose(stream);
 }
