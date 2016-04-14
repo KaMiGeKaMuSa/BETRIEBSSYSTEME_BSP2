7e6a78974670c24752bae9c3ccf732223a668e67
ZerOne10 <michi.mueller1709@gmail.com>
2016-04-14 16:36:03 +0200

    Update mypopen.h

--- mypopen.h
+++ mypopen.h
@@ -6,12 +6,13 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <errno.h>
-#include <string.h>
 
 #define READ_END 0
 #define WRITE_END 1
 
 FILE *mypopen(const char *command, const char *type);
 int mypclose(FILE *stream);
+static void childAction(int fd[2], int unused_end, int used_end, const char *command);
+static FILE * parentAction(int fd[2], int unused_end, int used_end, const char *type);
 
 #endif /* _MYPOPEN_H_ */
